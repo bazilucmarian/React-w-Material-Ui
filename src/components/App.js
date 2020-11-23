c8a3d785db2089e7447e7a0c50118beb4fc2ebab
bazilucmarian <bazilucmarian98@yahoo.com>
2020-11-23 12:29:00 +0200

    create footer section borrowing functionality from the header , when click to footer link change index for each link in header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./ui/Theme";
 import Header from "../components/ui/Header";
+import Footer from "../components/ui/Footer";
 
 function App() {
-  console.log(theme);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [value, setValue] = useState(0);
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Header />
+        <Header
+          value={value}
+          setValue={setValue}
+          selectedIndex={selectedIndex}
+          setSelectedIndex={setSelectedIndex}
+        />
         <Switch>
-          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route
+            exact
+            path="/"
+            component={() => <div style={{ height: "100vh" }}>Home</div>}
+          />
           <Route exact path="/services" component={() => <div>Services</div>} />
           <Route
             exact
@@ -33,6 +44,12 @@ function App() {
           <Route exact path="/contact" component={() => <div>Contact</div>} />
           <Route exact path="/estimate" component={() => <div>Estimate</div>} />
         </Switch>
+        <Footer
+          value={value}
+          setValue={setValue}
+          selectedIndex={selectedIndex}
+          setSelectedIndex={setSelectedIndex}
+        />
       </Router>
     </ThemeProvider>
   );
---
+++ src/components/ui/Footer.js
@@ -0,0 +1,260 @@
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import Grid from "@material-ui/core/Grid";
+import footerAdornment from "../../assets/Footer Adornment.svg";
+import facebook from "../../assets/facebook.svg";
+import twitter from "../../assets/twitter.svg";
+import instagram from "../../assets/instagram.svg";
+import Hidden from "@material-ui/core/Hidden";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.common.blue,
+    width: "100%",
+    zIndex: "1302",
+    position: "relative",
+  },
+  adornment: {
+    width: "25em",
+    verticalAlign: "bottom",
+    [theme.breakpoints.down("md")]: {
+      width: "21em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "15em",
+    },
+  },
+  mainContainer: {
+    position: "absolute",
+    top: "0",
+  },
+  link: {
+    color: "white",
+    fontFamily: "Arial",
+    fontSize: "0.8rem",
+    fontWeight: "bold",
+    textDecoration: "none",
+  },
+  gridItem: {
+    margin: "3em",
+  },
+  icon: {
+    height: "3em",
+    width: "3em",
+    [theme.breakpoints.down("xs")]: {
+      height: "2em",
+      width: "2em",
+    },
+  },
+  socialContainer: {
+    position: "absolute",
+    marginTop: "-6em",
+    right: "1.5em",
+    [theme.breakpoints.down("xs")]: {
+      right: "0.5em",
+    },
+  },
+}));
+
+const Footer = ({ setValue, setSelectedIndex }) => {
+  const classes = useStyles();
+
+  return (
+    <footer className={classes.footer}>
+      <Hidden mdDown>
+        <Grid container className={classes.mainContainer} justify="center">
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid
+                item
+                component={Link}
+                to="/"
+                className={classes.link}
+                onClick={() => setValue(0)}
+              >
+                Home
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid
+                item
+                component={Link}
+                to="/services"
+                className={classes.link}
+                onClick={() => {
+                  setValue(1);
+                  setSelectedIndex(0);
+                }}
+              >
+                Services
+              </Grid>
+              <Grid
+                item
+                component={Link}
+                to="/customsoftware"
+                className={classes.link}
+                onClick={() => {
+                  setValue(1);
+                  setSelectedIndex(1);
+                }}
+              >
+                Custom Software Development
+              </Grid>
+              <Grid
+                item
+                component={Link}
+                to="/mobileapps"
+                className={classes.link}
+                onClick={() => {
+                  setValue(1);
+                  setSelectedIndex(2);
+                }}
+              >
+                Mobile App Development
+              </Grid>
+              <Grid
+                item
+                component={Link}
+                to="/websites"
+                className={classes.link}
+                onClick={() => {
+                  setValue(1);
+                  setSelectedIndex(3);
+                }}
+              >
+                Website Development
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid
+                item
+                component={Link}
+                to="/revolution"
+                className={classes.link}
+                onClick={() => setValue(2)}
+              >
+                The Revolution
+              </Grid>
+              <Grid
+                item
+                component={Link}
+                to="/revolution"
+                className={classes.link}
+                onClick={() => setValue(2)}
+              >
+                Vision
+              </Grid>
+              <Grid
+                item
+                component={Link}
+                to="/revolution"
+                className={classes.link}
+                onClick={() => setValue(2)}
+              >
+                Technology
+              </Grid>
+              <Grid
+                item
+                component={Link}
+                to="/revolution"
+                className={classes.link}
+                onClick={() => setValue(2)}
+              >
+                Process
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid
+                item
+                component={Link}
+                to="/about"
+                className={classes.link}
+                onClick={() => setValue(3)}
+              >
+                About Us
+              </Grid>
+              <Grid
+                item
+                component={Link}
+                to="/about"
+                className={classes.link}
+                onClick={() => setValue(3)}
+              >
+                History
+              </Grid>
+              <Grid
+                item
+                component={Link}
+                to="/about"
+                className={classes.link}
+                onClick={() => setValue(3)}
+              >
+                Team
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid
+                item
+                component={Link}
+                to="/contact"
+                className={classes.link}
+                onClick={() => setValue(4)}
+              >
+                Contact us
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Hidden>
+      <img
+        src={footerAdornment}
+        alt="black decorative slash"
+        className={classes.adornment}
+      />
+      <Grid
+        container
+        justify="flex-end"
+        spacing={2}
+        className={classes.socialContainer}
+      >
+        <Grid
+          item
+          component={"a"}
+          href="http://www.facebook.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img alt="facebook logo" src={facebook} className={classes.icon} />
+        </Grid>
+        <Grid
+          item
+          component={"a"}
+          href="http://www.twitter.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img alt="twitter logo" src={twitter} className={classes.icon} />
+        </Grid>
+        <Grid
+          item
+          component={"a"}
+          href="http://www.instagram.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img alt="instagram logo" src={instagram} className={classes.icon} />
+        </Grid>
+      </Grid>
+    </footer>
+  );
+};
+export default Footer;
